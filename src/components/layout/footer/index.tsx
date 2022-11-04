b3a5cedc45d5079fcc9097af1b9427639c3a396f
Sheepeer <mattina_young@163.com>
2022-11-04 20:42:12 +0800

    merge: master

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -3,7 +3,20 @@ import styles from './style.module.scss'
 const Footer = () => {
   return (
     <div className={styles['footer-root']}>
-      All by sheepeer
+      <div className={styles['main']}>
+        All by sheepeer
+      </div>
+      <div className={styles['contact']}>
+        <div className={styles['board']}>
+          sheepeer@163.com
+        </div>
+        <div className={styles['social']}>
+          <span>github</span>
+          <span>tweeter</span>
+          <span>weibo</span>
+          <span>zhihu</span>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/pages/blog/index.tsx
+++ src/components/pages/blog/index.tsx
@@ -1,9 +1,17 @@
+import Tag from "../../basic/tag"
 import Container from "../../layout/container"
+import styles from './style.module.css'
 
 const BlogPage = () => {
   return (
     <Container pageTitle={"a blog"}>
-      a blog
+      <div className={styles['root']}>
+        <div className={styles['title']}>这里是标题标题标题标题标题</div>
+        <div className={styles['info']}>
+          <div className={styles['time']}>2022-01-02</div>
+          <Tag>标签</Tag>
+        </div>
+      </div>
     </Container>
   )
 }
