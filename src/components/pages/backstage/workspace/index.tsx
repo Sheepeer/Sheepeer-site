18ac80a848aed60bd6b49be545eb6c55db828137
yangchen7 <yangchen7@360.cn>
2023-01-31 17:18:33 +0800

    feat: add markdown editor

--- src/components/pages/backstage/workspace/index.tsx
+++ src/components/pages/backstage/workspace/index.tsx
@@ -1,9 +1,55 @@
+import MarkdownIt from 'markdown-it'
+import MdEditor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css';
+import Button from '@mui/material/Button';
+import Space from '@/components/basic/space';
+import { useState } from 'react';
+import Input from '@mui/material/Input';
 import styles from './style.module.scss'
 
+const mdParser = new MarkdownIt()
+
+type Content = {
+  html: string,
+  text: string
+}
+
 const WorkSpace = () => {
+  const [title, setTitle] = useState<string>()
+  const [content, setContent] = useState<Content>()
+
+  /**
+   * 这里要加上防抖
+   */
+  const titleChangeHandler = (e: any) => {
+    setTitle(e.target.value)
+  }
+  const contentChangeHandler = ({ html, text }: any) => {
+    setContent({ html, text })
+  }
+
   return (
-    <div>
-      WorkSpace - publish new post or new picture here
+    <div className={styles['root']}>
+      <div className={styles['header-wrapper']}>
+        <Space>
+          <Button variant='text'>发布</Button>
+          <Button >存草稿</Button>
+        </Space>
+      </div>
+      <div className={styles['editor-wrapper']}>
+        <div className={styles['title']}>
+          <Input
+            value={title}
+            onChange={titleChangeHandler}
+            placeholder='在此输入文章标题' />
+        </div>
+        <MdEditor
+          className={styles['editor']}
+          style={{}}
+          value={content?.text}
+          renderHTML={text => mdParser.render(text)}
+          onChange={contentChangeHandler} />
+      </div>
     </div>
   )
 }
