6c6884200a1d82f56fac7b380f190610f87a8cb5
Sheepeer <mattina_young@163.com>
2023-02-04 22:49:23 +0800

    style: footer

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -2,6 +2,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from './style.module.scss'
 
 const GITHUB_SRC = 'https://github.com/Sheepeer'
@@ -13,15 +14,21 @@ const Footer = () => {
     <div className={styles['footer-root']}>
       <div className={styles['main']}>
         All by sheepeer
+        sheepeer@163.com
       </div>
       <div className={styles['contact']}>
         <div className={styles['board']}>
-          sheepeer@163.com
+          <Image
+            className={styles['wechat']}
+            src={'/wechat.jpg'}
+            width={150}
+            height={150}
+            alt='wechat QR code' />
         </div>
         <div className={styles['social']}>
           <Link href={GITHUB_SRC}><GitHubIcon /></Link>
-          <Link href={INS_SRC}><InstagramIcon/></Link>
-          <Link href={TWITTER_SRC}><TwitterIcon/></Link>
+          <Link href={INS_SRC}><InstagramIcon /></Link>
+          <Link href={TWITTER_SRC}><TwitterIcon /></Link>
           <span>zhihu</span>
         </div>
       </div>
