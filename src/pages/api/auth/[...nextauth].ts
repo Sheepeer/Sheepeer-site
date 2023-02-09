27475a1be6c4698a2c722675845fbf8e165f00c0
yangchen7 <yangchen7@360.cn>
2023-02-09 10:26:03 +0800

    fix: delete callback api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,21 @@ const authOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!
     })
-  ]
+  ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      console.log(token, account)
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token, user }: any) {
+      console.log(session, token, user)
+      session.accessToken = token.access_token
+      return session
+    }
+  }
 }
 
 export default NextAuth(authOptions)
