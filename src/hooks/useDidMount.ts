af89a411e421b62b4cafec167d610e8c223d132e
yangchen7 <yangchen7@360.cn>
2023-02-09 12:26:54 +0800

    fix: rename _middleware.ts

--- src/hooks/useDidMount.ts
+++ src/hooks/useDidMount.ts
@@ -1,6 +1,7 @@
 import { EffectCallback, useEffect } from "react"
 
 const useDidMount = (callback: EffectCallback) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useEffect(callback, [])
 }
 
