75df6c1f74a6e60749aa47405e7559448442067b
yangchen7 <yangchen7@360.cn>
2023-04-26 11:51:09 +0800

    test: suffix

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,84 +1,85 @@
-import classNames from "classnames"
-import Image from "next/image"
-import Link from "next/link"
-import { useRouter } from "next/router"
-import Space from "../../basic/space"
-import styles from './style.module.scss'
-import LanguageBtn from "./language-btn"
-import { ReactNode, useState } from "react"
-import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
-import { useHeaderContent } from "src/context/headerContent"
-import React from "react"
+import classNames from "classnames";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Space from "../../basic/space";
+import styles from "./style.module.scss";
+import LanguageBtn from "./language-btn";
+import { ReactNode, useState } from "react";
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+import { useHeaderContent } from "src/context/headerContent";
+import React from "react";
 
-type HeaderMenu = Array<{ label: string, path: string }>
+type HeaderMenu = Array<{ label: string; path: string }>;
 export const MENU: HeaderMenu = [
-  { label: 'BLOGS', path: '/blogs' },
-  { label: 'GALLERY', path: '/gallery' },
-]
+  { label: "BLOGS", path: "/blogs" },
+  { label: "GALLERY", path: "/gallery" },
+];
 
 const HeaderMenu = () => {
-  const { pathname } = useRouter()
+  const { pathname } = useRouter();
 
   return (
-    <div className={styles['menu']}>
-      {
-        MENU.map(({ label, path }) => (
-          <Link key={label} href={path}>
-            <div className={classNames(
-              styles['menu-item'],
-              { [styles['menu-item__active']]: pathname === path }
-            )}>{label}</div>
-          </Link>)
-        )
-      }
-    </div >
-  )
-}
+    <div className={styles["menu"]}>
+      {MENU.map(({ label, path }) => (
+        <Link key={label} href={path}>
+          <div
+            className={classNames(styles["menu-item"], {
+              [styles["menu-item__active"]]: pathname === path,
+            })}
+          >
+            {label}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 const I18N_MENU = [
   {
-    label: '中文',
-    value: 'CN'
+    label: "中文",
+    value: "CN",
   },
   {
-    label: 'English',
-    value: 'EN'
-  }
-]
+    label: "English",
+    value: "EN",
+  },
+];
 
 interface Props {
-  style?: object
+  style?: object;
 }
 
 const Header = ({ style = {} }: Props) => {
-  const { isHeaderHidden, setIsHeaderHidden } = useHeaderContent()
+  const { isHeaderHidden, setIsHeaderHidden } = useHeaderContent();
 
-  const [suffix, setSuffix] = useState(false)
+  const [suffix, setSuffix] = useState(false);
 
   const clickSuffixHandler = () => {
     if (isHeaderHidden === false) {
-      setIsHeaderHidden(true)
-      setSuffix(true)
+      setIsHeaderHidden(true);
+      setSuffix(true);
     } else {
-      setIsHeaderHidden(false)
-      setSuffix(false)
+      setIsHeaderHidden(false);
+      setSuffix(false);
     }
-  }
+  };
 
   return (
-    <div className={styles['root']} style={style}>
+    <div className={styles["root"]} style={style}>
       <div
-        className={styles['header-wrapper']}
-        style={isHeaderHidden ? { height: 0, border: 'none' } : {}}
+        className={styles["header-wrapper"]}
+        style={isHeaderHidden ? { height: 0, border: "none" } : {}}
       >
         <div
-          className={styles['header']}
-          style={{ visibility: isHeaderHidden ? 'hidden' : 'visible' }}
+          className={styles["header"]}
+          style={{ visibility: isHeaderHidden ? "hidden" : "visible" }}
         >
-          <Link href={'/'}>
+          <Link href={"/"}>
             <Image
-              className={styles['logo']}
-              src={'/logo-final.jpg'}
+              className={styles["logo"]}
+              src={"/logo-final.jpg"}
               width={40}
               height={40}
               alt="Sheepeer's site"
@@ -91,12 +92,13 @@ const Header = ({ style = {} }: Props) => {
         </div>
       </div>
 
-      <div
-        className={styles['suffix']}
-        onClick={clickSuffixHandler}
-      >
+      <div className={styles["suffix"]} onClick={clickSuffixHandler}>
         {
-          suffix ? <span>A</span> : <span>B</span>
+          suffix ? (
+            <span className={styles["icon"]}>A</span>
+          ) : (
+            <span className={styles["icon"]}>B</span>
+          )
           // ? <React.Fragment><KeyboardArrowDown /> </React.Fragment>
           // : <React.Fragment><KeyboardArrowUp  /></React.Fragment>
           // ? <React.Fragment><KeyboardArrowDown className={styles['icon']} /> </React.Fragment>
@@ -107,7 +109,7 @@ const Header = ({ style = {} }: Props) => {
         } */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
