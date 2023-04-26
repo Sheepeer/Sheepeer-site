026145c7f409840b567cf9b63f79afb8bf45d9bf
yangchen7 <yangchen7@360.cn>
2023-04-26 15:03:45 +0800

    test: if 504

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -6,9 +6,9 @@ import Space from "../../basic/space";
 import styles from "./style.module.scss";
 import LanguageBtn from "./language-btn";
 import { ReactNode, useState } from "react";
-// import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material"; // this way importing mui icons will cause api 504
-import {Edit, Delete} from '@mui/icons-material'
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";  // this way won't
+import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material"; // this way importing mui icons will cause api 504
+import { Edit, Delete } from "@mui/icons-material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown"; // this way won't
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import { useHeaderContent } from "src/context/headerContent";
 import React from "react";
@@ -96,23 +96,25 @@ const Header = ({ style = {} }: Props) => {
       </div>
 
       <div className={styles["suffix"]} onClick={clickSuffixHandler}>
-        {suffix 
-          // ? (
-          //   <KeyboardArrowDownIcon className={styles["icon"]} />
-          // ) : (
-          //   <KeyboardArrowUpIcon className={styles["icon"]} />
-          // )
-          ? (
-            <Edit className={styles["icon"]} />
+        <KeyboardArrowDown />
+        <KeyboardArrowUp />
+        {
+          suffix ? (
+            <KeyboardArrowDownIcon className={styles["icon"]} />
           ) : (
-            <Delete className={styles["icon"]} />
+            <KeyboardArrowUpIcon className={styles["icon"]} />
           )
-        //   ?(
-        //   <KeyboardArrowDown className={styles["icon"]} />
-        // ) : (
-        //   <KeyboardArrowUp className={styles["icon"]} />
-        // )
-      }
+          // ? (
+          //   <Edit className={styles["icon"]} />
+          // ) : (
+          //   <Delete className={styles["icon"]} />
+          // )
+          //   ?(
+          //   <KeyboardArrowDown className={styles["icon"]} />
+          // ) : (
+          //   <KeyboardArrowUp className={styles["icon"]} />
+          // )
+        }
         {/* {
           suffix ? 'show' : 'hide'
         } */}
