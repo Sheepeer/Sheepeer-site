93b01d462233d995dc5567c15dea40bf88e5fc22
yangchen7 <yangchen7@360.cn>
2023-04-26 15:17:56 +0800

    test: language btn

--- src/components/layout/header/language-btn/index.tsx
+++ src/components/layout/header/language-btn/index.tsx
@@ -1,25 +1,25 @@
-import { Button, Menu, MenuItem } from "@mui/material"
-import { useRouter } from "next/router"
-import { useState } from "react"
-import LanguageIcon from '@mui/icons-material/Language';
-import styles from './style.module.scss'
-
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useRouter } from "next/router";
+import { useState } from "react";
+// import LanguageIcon from '@mui/icons-material/Language';
+import { Language } from "@mui/icons-material";
+import styles from "./style.module.scss";
 
 const I18N_MENU = [
   {
-    label: '中文',
-    value: 'CN'
+    label: "中文",
+    value: "CN",
   },
   {
-    label: 'English',
-    value: 'EN'
-  }
-]
+    label: "English",
+    value: "EN",
+  },
+];
 
 const LanguageBtn = () => {
-  const router = useRouter()
+  const router = useRouter();
 
-  const [language, setLanguage] = useState(I18N_MENU[0].value)
+  const [language, setLanguage] = useState(I18N_MENU[0].value);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
@@ -32,46 +32,46 @@ const LanguageBtn = () => {
   };
 
   const clickMenuItemHandler = (value: string) => {
-    setLanguage(value)
-    handleClose()
-  }
+    setLanguage(value);
+    handleClose();
+  };
 
   return (
-    <div className={styles['root']}>
+    <div className={styles["root"]}>
       <Button
-        id='menu-trigger'
+        id="menu-trigger"
         variant="text"
-        aria-controls={open ? 'menu' : undefined}
+        aria-controls={open ? "menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
+        aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
-        startIcon={<LanguageIcon />}
+        // startIcon={<LanguageIcon />}
+        startIcon={<Language/>}
       >
         {language}
       </Button>
 
       <Menu
-        id='menu'
+        id="menu"
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-
         MenuListProps={{
-          'aria-labelledby': 'menu-trigger',
+          "aria-labelledby": "menu-trigger",
         }}
       >
-        {
-          I18N_MENU.map(({ label, value }) => (
-            <MenuItem
-              key={value}
-              onClick={() => clickMenuItemHandler(value)}
-              // onClick={handleClose}
-            >{label}</MenuItem>
-          ))
-        }
+        {I18N_MENU.map(({ label, value }) => (
+          <MenuItem
+            key={value}
+            onClick={() => clickMenuItemHandler(value)}
+            // onClick={handleClose}
+          >
+            {label}
+          </MenuItem>
+        ))}
       </Menu>
     </div>
-  )
-}
+  );
+};
 
-export default LanguageBtn
+export default LanguageBtn;
