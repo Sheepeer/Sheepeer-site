2240d0b822943dd0470d168cf651b8686f45bdd2
Sheepeer <mattina_young@163.com>
2023-05-16 15:10:39 +0800

    feat: add friend link

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,53 +1,45 @@
-import { GitHub, AttachEmail, QuestionAnswer } from '@mui/icons-material';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import Link from 'next/link';
-import Image from 'next/image';
-import styles from './style.module.scss'
-import { MENU } from '../header';
-import Contact from './contact';
+import { GitHub, AttachEmail, QuestionAnswer } from "@mui/icons-material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./style.module.scss";
+import { MENU } from "../header";
+import Contact from "./contact";
 
-const GITHUB_SRC = 'https://github.com/Sheepeer'
-const EMAIL = 'mattinayoung@gmail.com'
-const WECHAT = 'Mattina_young'
-const INS_SRC = ''
-const TWITTER_SRC = ''
+const GITHUB_SRC = "https://github.com/Sheepeer";
+const EMAIL = "mattinayoung@gmail.com";
+const WECHAT = "Mattina_young";
+const INS_SRC = "";
+const TWITTER_SRC = "";
 
-const FRIEND_LINKS: string[] = [
-  // 'xxx.com',
-  // 'xxxxx.com.cn',
-  // 'xxxx.com.cn'
-]
+const FRIEND_LINKS: string[] = ["https://blog.itswincer.com"];
 
 const CONTACT_LIST = [
   { icon: <GitHub />, link: GITHUB_SRC },
   { icon: <AttachEmail />, link: EMAIL },
-  { icon: <QuestionAnswer />, link: WECHAT }
-]
+  { icon: <QuestionAnswer />, link: WECHAT },
+];
 
 const Footer = () => {
   return (
-    <div className={styles['footer-root']}>
+    <div className={styles["footer-root"]}>
       <Contact list={CONTACT_LIST} />
-      <div className={styles['main']}>
-        <div className={styles['title']}>Menu</div>
-        {
-          MENU.map(({ path, label }) => (
-            <Link href={path} key={label}>
-              <div className={styles['item']}>{label}</div>
-            </Link>
-          ))
-        }
+      <div className={styles["main"]}>
+        <div className={styles["title"]}>Menu</div>
+        {MENU.map(({ path, label }) => (
+          <Link href={path} key={label}>
+            <div className={styles["item"]}>{label}</div>
+          </Link>
+        ))}
       </div>
-      <div className={styles['main']}>
-        <div className={styles['title']}>Friend Links</div>
-        {
-          FRIEND_LINKS.map(item => (
-            <Link href={item} key={item}>
-              <div className={styles['item']}>{item}</div>
-            </Link>
-          ))
-        }
+      <div className={styles["main"]}>
+        <div className={styles["title"]}>Friend Links</div>
+        {FRIEND_LINKS.map((item) => (
+          <a href={item} target="blank" className={styles["item"]} key={item}>
+            {item}
+          </a>
+        ))}
       </div>
       {/* <div className={styles['contact']}>
         <div className={styles['board']}>
@@ -65,9 +57,9 @@ const Footer = () => {
           <span>zhihu</span>
         </div>
       </div> */}
-      <div className={styles['p']}>© From 2022, created by Sheepeer</div>
+      <div className={styles["p"]}>© From 2022, created by Sheepeer</div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
